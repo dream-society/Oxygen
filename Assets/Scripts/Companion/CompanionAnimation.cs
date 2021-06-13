40978a00a3c968dd13dcc0e15ddc1a1a56a44bf2
Michele Rossi <mikymod@gmail.com>
2021-06-13 12:17:35 +0200

    add companion animations

---
+++ Assets/Scripts/Companion/CompanionAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    private PlayerMovement playerMovement;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("isIdle", Mathf.Abs(rb.velocity.x) == 0 || playerMovement.MovementInput.x == 0 && playerMovement.Grounded);
+        animator.SetBool("isWalking", Mathf.Abs(rb.velocity.x) > 0 || (playerMovement.MovementInput.x == 0 && playerMovement.Grounded));
+        animator.SetBool("inAir", playerMovement.enabled && !playerMovement.Grounded);
+    }
+}
