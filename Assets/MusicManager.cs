1ad7924e15db9dfff8995e673bcb7f35318e2c18
Michele Rossi <mikymod@gmail.com>
2021-06-13 17:35:26 +0200

    add music loop

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+}
